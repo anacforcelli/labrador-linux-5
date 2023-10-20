ebdbd5ae56e1a85969764c3fb5a98d59cfc0ae40
anacforcelli <anacforcelli@gmail.com>
2023-10-20 11:35:03 -0300

    reset workaround on PMIC

--- linux-source-5.10/drivers/mfd/atc260x/atc260x-i2c.c
+++ linux-source-5.10/drivers/mfd/atc260x/atc260x-i2c.c
@@ -127,6 +127,20 @@ static int atc260x_i2c_probe(struct i2c_client *i2c,
 		return ret;
 	}
 	
+	switch (atc260x->ic_type)
+	{
+	case ATC260X_ICTYPE_2603C:
+		/* TEMPORARY FIX FOR LORA */
+		atc260x_reg_write(atc260x, ATC2603C_MFP_CTL, BIT(8) & ~BIT(7));
+		atc260x_reg_write(atc260x, ATC2603C_GPIO_OUTEN, BIT(4)); //enable sgpio4
+		atc260x_reg_write(atc260x, ATC2603C_GPIO_DAT, BIT(4));
+		udelay(200);
+		atc260x_reg_write(atc260x, ATC2603C_GPIO_DAT, 0);
+		break;
+	default:
+		break;
+	}
+
 	dev_info(&i2c->dev, "probe finished\n");
 	return 0;
 }
