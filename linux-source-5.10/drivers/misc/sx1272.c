ac867b21dfc8e521909ecc53416dbdac6eb1eb82
anacforcelli <anacforcelli@gmail.com>
2024-01-15 17:46:26 -0300

    fixed sx1272 driver skeleton, now the version is read on base 3.1

--- linux-source-5.10/drivers/misc/sx1272.c
+++ linux-source-5.10/drivers/misc/sx1272.c
@@ -1,61 +1,109 @@
 #include <linux/spi/spi.h>
 #include <linux/property.h>
 #include <linux/module.h>
+#include <linux/device.h>
+#include <linux/of.h>
 
-#define SPI_MODE 0
-#define SPI_MAX_SPEED_HZ 100000
-#define SPI_BITS_PER_WORD 8
-
-struct sx1272 {
-	struct spi_device *spi;
+struct sx1272_chip {
+	struct mutex lock;
 };
 
-static int sx1272_probe(struct spi_device *spi) {
+#define REG_VERSION 0x42
 
-    int err;
-    uint8_t data; 
-    uint8_t tx = 0x00;
-
-    struct spi_transfer tr = {
-        .rx_buf = &data,
-        .tx_buf = &tx,
-        .len = 1,
-    };
-    
-
-    // Setup SPI
-    spi->mode = SPI_MODE;
-    spi->max_speed_hz = SPI_MAX_SPEED_HZ;
-    spi->bits_per_word = SPI_BITS_PER_WORD;
-
-    err = spi_setup(spi);
-    if(err < 0){
-        pr_err("spi_setup failed\n");
-        return err;
-    }
-    err = spi_sync_transfer(spi, &tr, 1);
-    if (err != 0){
-        pr_err("spi xfer failed\n");
-        return err;
-    }
-    pr_info("rx: %x\n", data);
-
-    return 0; 
+static int sx1272_read_reg8(struct spi_device *spi, u8 reg, u8 *val)
+{
+	reg = reg & ~BIT(7); /* clear bit 7 to read reg */
+	return spi_write_then_read(spi, &reg, 1, val, 1);
 }
 
-static const struct spi_device_id caninos_ids[] = {
-	{"caninos,sx1272", 0},
+static int sx1272_write_reg8(struct spi_device *spi, u8 reg, u8 val)
+{
+	u8 buf[2];
+	buf[0] = reg | BIT(7); /* set bit 7 to write reg */
+	buf[1] = val;
+	return spi_write(spi, buf, 2);
+}
+
+static int sx1272_probe(struct spi_device *spi)
+{
+	struct device *dev = &spi->dev;
+	struct sx1272_chip *sx1272;
+	int ret;
+	u8 val;
+
+	sx1272 = devm_kzalloc(dev, sizeof(*sx1272), GFP_KERNEL);
+	
+	if (!sx1272) {
+		return -ENOMEM;
+	}
+	
+	spi_set_drvdata(spi, sx1272);
+	
+	mutex_init(&sx1272->lock);
+	
+	dev_info(dev, "Hello from sx1272 probe!!\n");
+	
+	/* Configure the SPI bus */
+	spi->mode = (SPI_MODE_0);
+	spi->bits_per_word = 8;
+	spi->max_speed_hz = 100000;
+	
+	ret = spi_setup(spi);
+	
+	if (ret) {
+		dev_err(dev, "spi setup failed\n");
+		return ret;
+	}
+	
+	ret = sx1272_read_reg8(spi, REG_VERSION, &val);
+	
+	if (ret) {
+		dev_err(dev, "unable to read chip version\n");
+		return ret;
+	}
+	
+	/* val should be 0x22 for sx1272 */
+	dev_info(dev, "Chip version is 0x%x\n", val);
+	
+	return 0;
+}
+
+static int sx1272_remove(struct spi_device *spi)
+{
+	struct sx1272_chip *sx1272 = spi_get_drvdata(spi);
+	
+	mutex_destroy(&sx1272->lock);
+	
+	return 0;
+}
+
+#ifdef CONFIG_OF
+static const struct of_device_id sx1272_of_id[] = {
+	{ .compatible = "caninos,sx1272" },
 	{ }
 };
-MODULE_DEVICE_TABLE(spi, caninos_ids);
+MODULE_DEVICE_TABLE(of, sx1272_of_id);
+#endif
+
+static const struct spi_device_id sx1272_spi_id[] = {
+	{ "sx1272" },
+	{ }
+};
+
+MODULE_DEVICE_TABLE(spi, sx1272_spi_id);
 
 static struct spi_driver sx1272_driver = {
-    .driver= { .name = "caninos-lora"},
-	.probe		= sx1272_probe,
-    .id_table = caninos_ids,
+	.driver = {
+		.name = "sx1272",
+		.of_match_table = of_match_ptr(sx1272_of_id),
+	},
+	.probe    = sx1272_probe,
+	.remove   = sx1272_remove,
+	.id_table = sx1272_spi_id,
 };
 
 module_spi_driver(sx1272_driver);
 
+MODULE_AUTHOR("Ana");
 MODULE_DESCRIPTION("SX1272 Driver");
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL v2");
